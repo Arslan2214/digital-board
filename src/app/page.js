686cb138961cf3090272cb57b9e54a1839b9887d
ARslan_Ahm <mycomputer2214@gmail.com>
2024-10-24 21:06:16 +0500

    Add Working Portotype for real project.
    There are some basic tools included and also working fine but not ready yet to publish.

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import DigitalBoard from '@/components/DigitalBoard'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between pt-20">
+      <DigitalBoard />
+    </main>
+  )
+}
